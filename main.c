80bf725159886a98b518ef3b440a8bdcb9d3127f
masonsquier <squier.mason@gmail.com>
2024-01-02 14:59:40 -0500

    Added new output to main.c

--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 
 int main () {
      printf("Hello World From vs\n");       
-         printf("I am Mason Squier\n");   
+         printf("I am Mason Squier\n"); 
+    printf("Another output lol");
 }
